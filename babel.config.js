5919d919dbbb13f4e4742ca712c868fcdd4c3449
Serdar MSC <serdarmsc@gmail.com>
2026-07-07 21:23:23 +0300

    Refactor babel.config.js presets configuration

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          unstable_transformImportMeta: true,
+          native: {
+            unstable_transformProfile: "default",
+          },
+        },
+      ],
+    ],
   };
 };
