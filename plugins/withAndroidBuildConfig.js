d40a24a1c449d184c1e729a60c9b166a7c1b901f
Serdar MSC <serdarmsc@gmail.com>
2026-07-08 23:12:06 +0300

    Add files via upload

--- plugins/withAndroidBuildConfig.js
+++ plugins/withAndroidBuildConfig.js
@@ -1,5 +1,12 @@
 const { withAppBuildGradle } = require("@expo/config-plugins");
 
+/**
+ * Recent Android Gradle Plugin versions (8.x) disable BuildConfig class
+ * generation by default. Expo's generated MainActivity.kt / MainApplication.kt
+ * reference BuildConfig.IS_NEW_ARCHITECTURE_ENABLED / IS_HERMES_ENABLED, which
+ * then fails with "Unresolved reference 'BuildConfig'" during release builds.
+ * This plugin explicitly re-enables buildConfig generation in app/build.gradle.
+ */
 module.exports = function withAndroidBuildConfig(config) {
   return withAppBuildGradle(config, (config) => {
     if (config.modResults.language === "groovy") {
