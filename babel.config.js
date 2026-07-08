43fa57c845a7ce0c3607ea1cf12a0f240d815aeb
Serdar MSC <serdarmsc@gmail.com>
2026-07-08 23:09:21 +0300

    Add files via upload

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          unstable_transformImportMeta: true,
+          // Force Babel to downcompile modern class syntax (private fields,
+          // etc.) instead of assuming the target Hermes build supports it
+          // natively. Some react-native core source (e.g. DOMRectReadOnly.js)
+          // uses private class fields that this project's bundled Hermes
+          // compiler fails to parse ("private properties are not supported").
+          native: {
+            unstable_transformProfile: "default",
+          },
+        },
+      ],
+    ],
   };
 };
